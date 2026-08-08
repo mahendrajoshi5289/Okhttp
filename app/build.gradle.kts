526c293f130c93744c0b4755461f63bd126af86b
mahendrajoshi5289 <167439941+mahendrajoshi5289@users.noreply.github.com>
2026-08-08 14:12:27 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.okhttpandroid"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.okhttpandroid"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
