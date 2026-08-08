899d4d492608bcfe23672d7172e048f5b2cd8fad
mahendrajoshi5289 <167439941+mahendrajoshi5289@users.noreply.github.com>
2026-08-08 14:19:22 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/okhttpandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.okhttpandroid
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
